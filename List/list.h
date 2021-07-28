4e11d634c7b46cab5e0ba4ffe25c339918982012
zhenli1347 <2503248255@qq.com>
2021-07-28 09:53:58 +0800

    Update list.h

--- List/list.h
+++ List/list.h
@@ -5,7 +5,8 @@ struct List{
 	int vertex;
 	struct List *next;
 };
-typedef struct List *llink;//将 结构指针类型定义为 llink，可用于链表的头指针。
+typedef struct List *llink;//将链表结构指针类型定义为 llink，可用于链表的头指针
+							//后续创建的链表包含头节点(不包含链表的具体内容)和首元结点（链表的第一个数据结点）
 			   
 
 void insert_list(llink *,int);
